0a7795728517742ceabca1fbc72d918d502e5d52
Ibby <neon7860@gmail.com>
2025-05-07 19:36:25 +0100

    chore: add today default, focus ring, run prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
-import './App.css'
-import TripForm from './features/trip-form/TripForm'
+import "./App.css";
+import TripForm from "./features/trip-form/TripForm";
 
 function App() {
-
   return (
     <div>
       <TripForm />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
